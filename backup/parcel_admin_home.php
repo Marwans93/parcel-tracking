3cb41b93eadb03f20decc9d1722aff3c52184e4f
Wan Mohamad Ridhwan Bin Khaldin <ridhz93@yahoo.com>
2018-10-05 20:57:24 +0800

    Add files via upload

---
+++ backup/parcel_admin_home.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<table width="99%" height="447" border="1">
+	<tr>
+		<td height="76" colspan="2"><?php include 'abc_header.php';?></td>				
+	</tr>
+	<tr>
+		<td width="14%" height="321"><?php include'parcel_admin_menu.php';?></td><!--menu section-->
+		<td width="667" align="left" valign="top"><!--content section-->
+		<H1>This is a content of ABC Bookstore</H1>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2"><?php include'abc_copyright.php';?></td>
+	</tr>
+	</table>
+</body>
+</html>
---
+++ backup/parcel_admin_menu.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<p>MENU LIST</p>
+<p>&nbsp;</p>
+<p><a href="parcel_list.php" title="View Book">Parcel List</a></p>
+<p><a href="User_list.php">User List</a></p>
+<p><a href="Search.php">Search</a></p>
+<p>&nbsp;</p>
+<p><a href="abc_search.php">Sign Out</a></p>
+<p>&nbsp;</p>
+</body>
+</html>
